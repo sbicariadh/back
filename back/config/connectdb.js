a7e779275f86838e792bb8df3817319b1679c608
sbicariadh <sbicataha@gmail.com>
2023-03-02 21:21:14 +0100

    vf

--- back/config/connectdb.js
+++ back/config/connectdb.js
@@ -3,7 +3,7 @@ const mongoose = require ('mongoose')
 const connectDB = async ()=>{
     try{
         await mongoose.connect('mongodb+srv://sbica:mNKgXX47PpwWF6Fp@cluster0.5psipxh.mongodb.net/?retryWrites=true&w=majority')
-        console.log('connected to DB')
+        console.log('connected to Data Base')
     }catch(err){
         console.log (err)
 
